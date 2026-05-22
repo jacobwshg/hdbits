3fc93d347bcd51d0fe866ca2d30a94e31ae2800c
jacobwshg <wsg438@gmail.com>
2026-05-22 01:47:48 -0500

    count15

--- count15.sv
+++ count15.sv
@@ -1,10 +1,10 @@
 
 /*
- * Build a decade counter that counts from 0 through 9, inclusive, with a period of 10.
+ * Build a 4-bit binary counter that counts from 0 through 15, inclusive, with a period of 16.
  * The reset input is synchronous, and should reset the counter to 0.
  */
 
-module top_module(
+module top_module (
 	input clk,
 	input reset, // Synchronous active-high reset
 	output [3:0] q
@@ -13,8 +13,8 @@ module top_module(
 	always_ff @ ( posedge clk )
 	begin
 		if ( reset ) q <= 4'h0;
-		else q <= ( q===4'h9 ) ? 4'h0: ( q + 1'h1 );
+		else q <= q + 1'h1;
 	end
-	
+
 endmodule
 
